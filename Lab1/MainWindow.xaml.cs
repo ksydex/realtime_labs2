cf3bc50f2138654826724af237e276c64e4e5981
Artyom Lukyanov <artglz63@gmail.com>
2022-05-26 03:52:18 +0300

    init

---
+++ Lab1/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Labs2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly List<string> ProcessNames = new() { "notepad", "mspaint" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            ProcessNames.ForEach(name =>
+            {
+                var p = new Process();
+                p.StartInfo.FileName = name;
+                p.Exited += (o, ea) =>
+                    Dispatcher.Invoke(() => { Process.Start("calc"); });
+                p.EnableRaisingEvents = true;
+                p.Start();
+            });
+        }
+    }
+}
